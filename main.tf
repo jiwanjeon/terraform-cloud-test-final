601da11b5602a43bbdf68699e68f427e0d80fee1
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-09-28 15:52:54 +0900

    Upload main and state files

---
+++ main.tf
@@ -0,0 +1,34 @@
+# terraform {
+#   cloud {
+#     organization = "jeonjiwan"
+
+#     workspaces {
+#       name = "terraform-cloud-test-Final"
+#     }
+#   }
+# }
+provider "aws" {
+  region = "ap-northeast-2"
+  access_key = "AKIA5T5J5373TD2GK5WG"
+  secret_key = "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"
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id = "vpc-05397ee1bcd1b5f72"
+  cidr_block = "10.10.7.0/24"
+  availability_zone =  "ap-northeast-2a"
+
+  tags = {
+    Name = "(Before)Terraform-Cloud-Test-1"
+  }
+}
+
+resource "aws_subnet" "subnet-2" {
+  vpc_id = "vpc-05397ee1bcd1b5f72"
+  cidr_block = "10.10.8.0/24"
+  availability_zone =  "ap-northeast-2a"
+
+  tags = {
+    Name = "(Before)Terraform-Cloud-Test-1"
+  }
+}
