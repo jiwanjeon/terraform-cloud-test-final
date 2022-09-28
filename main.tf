5daa06db6bc111a4d2aae10c7c1380883b67c1a9
jjeon17 <33076677+Jonathan940110@users.noreply.github.com>
2022-09-28 15:53:49 +0900

    Make feat/prod branch

--- main.tf
+++ main.tf
@@ -13,6 +13,7 @@ provider "aws" {
   secret_key = "HrqqZIRTqgUQRqAzoKtQd8mSo/FMAmeukDiBHzQ+"
 }
 
+//1
 resource "aws_subnet" "subnet-1" {
   vpc_id = "vpc-05397ee1bcd1b5f72"
   cidr_block = "10.10.7.0/24"
@@ -23,6 +24,7 @@ resource "aws_subnet" "subnet-1" {
   }
 }
 
+//2
 resource "aws_subnet" "subnet-2" {
   vpc_id = "vpc-05397ee1bcd1b5f72"
   cidr_block = "10.10.8.0/24"
